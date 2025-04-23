a4149cf1ddb5c12ac72c7e223489d5d37d8eaec8
ShanwillS <99748818+ShanwillS@users.noreply.github.com>
2025-04-23 14:14:21 +0200

    Fixed comments

--- BankWithdrawalAPI/Program.cs
+++ BankWithdrawalAPI/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Add dependency injections here later (e.g. services, repositories, etc.)
+// Add dependency injections
 builder.Services.AddSingleton<IAccountRepository, AccountRepository>();
 builder.Services.AddSingleton<IEventPublisher, SnsEventPublisher>();
 builder.Services.AddScoped<IBankAccountService, BankAccountService>();
